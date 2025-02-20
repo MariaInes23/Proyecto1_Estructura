aa8bf14a3ccc591befef3f4b9682069fb4590176
María Inés Cardona López <ines7r@gmail.com>
2025-02-19 22:26:05 -0600

    Getter y Setter
    En Clase Mantenimiento

--- Mantenimiento.py
+++ Mantenimiento.py
@@ -3,3 +3,38 @@ class ClaseMantenimiento:
         self.fecha = fecha
         self.descripcion = descripcion
         self.costo = costo
+
+
+    @property
+    def fecha(self):
+        return self._fecha
+
+    @fecha.setter
+    def fecha(self, valor):
+        if isinstance(valor, str):
+            self._fecha = valor
+        else:
+            raise ValueError('La fecha es invalida')
+        
+    
+    @property
+    def descripcion(self):
+        return self._descripcion
+
+    @descripcion.setter
+    def descripcion(self, valor):
+        if isinstance(valor, str):
+            self._descripcion = valor
+        else:
+            raise ValueError('La descripción debe ser una cadena de texto.')
+
+    @property
+    def costo(self):
+        return self._costo
+
+    @costo.setter
+    def costo(self, valor):
+        if isinstance(valor, (int, float)) and valor > 0:
+            self._costo = valor
+        else:
+            raise ValueError('El costo debe ser un número positivo.')
